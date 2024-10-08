bd1815472622a0b7a06e10f5eec92cb2f426d950
Nlucky320 <luckyes144@gmail.com>
2024-10-08 13:48:09 +0600

    new commit

--- problem.js
+++ problem.js
@@ -10,24 +10,31 @@ console.log(greet);
 /* You get an array of numbers, return the sum of all of the positives ones.
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
- */
-function positiveSum(arr) {
-  return arr.reduce(
-    (accumulator, currentValue) =>
-      currentValue > 0 ? accumulator + currentValue : accumulator,
-    0
-  );
-}
+//  */
+// function positiveSum(arr) {
+//   return arr.reduce(
+//     (accumulator, currentValue) =>
+//       currentValue > 0 ? accumulator + currentValue : accumulator,
+//     0
+//   );
+// }
 
-function positiveSum(arr) {
-  var total = 0;
-  for (i = 0; i < arr.length; i++) {
+// function positiveSum(arr) {
+//   var total = 0;
+//   for (i = 0; i < arr.length; i++) {
 
-    if (arr[i] > 0) {
+//     if (arr[i] > 0) {
   
-      total += arr[i];
-    }
-  }
-  return total; 
+//       total += arr[i];
+//     }
+//   }
+//   return total; 
+// }
+// console.log(positiveSum([]));
+
+
+function noSpace(x) {
+  const removeSpace = x.split(' ').join('')
+  return removeSpace;
 }
-console.log(positiveSum([]));
+console.log(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
