01f5e3a75817ec68b8171214291d13c96d57fef9
Nlucky320 <luckyes144@gmail.com>
2024-10-09 12:15:07 +0600

    new commit

--- problem.js
+++ problem.js
@@ -170,4 +170,33 @@ return arr.reduce(
 } */
 
 // console.log(findSmallestInt([34, -345, -1, 100]));
-  
+/*   Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+ */
+
+// function past(h, m, s) {
+//   //#Happy Coding! ^_^
+
+
+// }
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+console.log(makeUpperCase('lucky'))
