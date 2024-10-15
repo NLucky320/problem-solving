9ec5747fc3eb15836b9fe7befc5b10f39fef9530
Nlucky320 <luckyes144@gmail.com>
2024-10-15 21:58:32 +0600

    new commit

--- codewars.js
+++ codewars.js
@@ -343,10 +343,28 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 function countPositivesSumNegatives(input) {
   // your code here
+  let countPositives = 0;
+  let sumNegatives = 0;
+  for (let i = 0; i < input.length; i++){
+ 
+    if (input[i] > 0) {
+
+      countPositives++;
+    }
+    else {
+    sumNegatives+=input[i]
+    }
+  }
+  return [countPositives,sumNegatives];
+
 }
 
 
-
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
 
 
 
