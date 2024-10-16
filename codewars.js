f9961f9c374617810b25bc75dafce327a30ced4c
Nlucky320 <luckyes144@gmail.com>
2024-10-16 19:45:23 +0600

    new commit

--- codewars.js
+++ codewars.js
@@ -428,13 +428,33 @@ Write a function to return the 3rd.
 
 Note: only positive integers will be tested. */
 
-
+/* 
 function otherAngle(a, b) {
 
   return (180 - (a + b));
   
 }
-console.log(otherAngle(10,20))
+console.log(otherAngle(10,20)) */
+
+
+/* The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer. */
+
+function cockroachSpeed(s) {
+  //Good Luck!
+}
+
+
+
+
+
+
+
+
 
 /* Return the number (count) of vowels in the given string.
 
